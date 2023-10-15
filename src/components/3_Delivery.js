4eb4f47e59118cc17255243b3fe830ea4c144276
Nikolenko Tymofii <nikolenkote@gmail.com>
2023-10-16 01:39:36 +0200

    1

--- src/components/3_Delivery.js
+++ src/components/3_Delivery.js
@@ -1,24 +1,27 @@
 import React from "react";
-import iphone_photo from "./img/FC_two_phones_1.png";
+import iphone_photo_1 from "./img/iphones/FC_two_phones_1.png";
+import iphone_photo_2 from "./img/iphones/FC_two_phones_2.png";
+import nikolenkote from "./img/NIKOLENKOTE.png";
+
 
 const Delivery = () => {
   return (
-    <div className="w-full dg-white py-7 px-4 ">
+    <div className="w-full dg-white py-7 px-4">
       <h3 className="flex justify-center">
-        <span className=" bg-indigo-500 relative  rounded-full h-9 shadow-md">
+        <span className=" bg-indigo-500 relative text-center  rounded-full h-9 shadow-md w-content">
           <span className="relative text-white font-bold text-2xl px-3 ">
-            Example of my works
+          You can view my work and order my services below.
           </span>
         </span>
       </h3>
-      <div className="w-flex h-flex max-w-[1000px] mx-auto grid md:grid-cols-2 bg-orange-300">
-        <img className="mx-1" src={iphone_photo} alt="iphone_photo" />
-        <div className="flex-col grid bg-orange-500 m-1">
-          <h1 className="md:text-4xl sm:text-3xl text-2xl text-center font-bold py-2 m-auto bg-orange-200 ">
+      <div className="w-flex h-flex max-w-[1000px] mx-auto grid md:grid-cols-2 mt-2">
+        <img className="mx-1" src={iphone_photo_1} alt="iphone_photo" />
+        <div className="flex-col grid m-1">
+          <h1 className="md:text-4xl sm:text-3xl text-2xl text-center font-bold py-2 m-auto">
             Don't miss the opportunity to place your order today
           </h1>
           <p className="px-3 text-2xl text-justify">
-            For several years I provided photography services in Ukraine. And
+          <img className="mx-1 h-[100px] inline-flex items-baseline" src={nikolenkote} alt="iphone_photo"/>For several years I provided photography services in Ukraine. And
             now don’t miss the opportunity to order my services in Berlin. I
             provide the services of a reportage and artistic
             photography is also available. You only need to choose a studio. I
@@ -26,7 +29,7 @@ const Delivery = () => {
             your photographs that are damaged, scratched, or discolored, even
             after washing in the washing machine. You can view my work and order my services below.
           </p>
-          <div className="flex items-center justify-center bg-green-600">
+          <div className="flex items-center justify-center">
             <button className="bg-black  text-[#00df9a] h-[50px] w-[200px] rounded-xl font-medium ">
               ORDER TODAY
             </button>
