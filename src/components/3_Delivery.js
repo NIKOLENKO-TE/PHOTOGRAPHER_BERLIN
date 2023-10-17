54b560e6c39eaef22916c2128fec4d7ffa234ab8
Nikolenko Tymofii <nikolenkote@gmail.com>
2023-10-17 14:10:19 +0200

    бедый фон у описания

--- src/components/3_Delivery.js
+++ src/components/3_Delivery.js
@@ -6,16 +6,16 @@ const Delivery = () => {
   return (
     <div className="w-full dg-white py-7 px-4">
       <h3 className="flex justify-center">
-        <span className=" bg-indigo-500 relative text-center  rounded-full h-9 shadow-md w-content">
+        <span className=" bg-indigo-500 relative text-center rounded-full h-9 shadow-md w-content">
           <span className="relative text-white font-bold text-2xl px-3 ">
             You can order my services below.
           </span>
         </span>
       </h3>
-      <div className="w-flex h-flex max-w-[1000px]  mx-auto grid md:grid-cols-2 mt-2">
-        <img className="mx-1 " src={iphone_photo_1} alt="iphone_photo" />
+      <div className="w-flex h-flex max-w-[1000px]  mx-auto py-2 pl-4 grid md:grid-cols-2 mt-2 bg-white/50 rounded-2xl shadow-xl">
+        <img className="mx-1 py-2 " src={iphone_photo_1} alt="iphone_photo " />
         <div className="flex-col grid m-1 ">
-          <h1 className="text-center md:text-4xl  sm:text-3xl text-2xl font-bold ml-2 mr-2  ">
+          <h1 className="text-center md:text-4xl  sm:text-3xl text-2xl font-bold ml-2 mr-2 pr-2 ">
             Don't miss the opportunity to place your order today
           </h1>
           <p className="px-3 text-2xl text-justify tracking-tight leading-8">
@@ -28,11 +28,11 @@ const Delivery = () => {
             now don’t miss the opportunity to order my services in Berlin. I
             provide the services of a reportage and artistic photography is also
             available. You only need to choose a studio. I also have extensive
-            experience in photo restoration. I can restore your photographs that
+            experience in photo restoration. I can restore your photographs, negatives photos that
             are damaged, scratched, or discolored, even after washing in the
             washing machine. You can view my work and order my services below.
           </p>
-          <div className="flex items-center justify-center ">
+          <div className="flex items-center justify-center py-2">
             <button className="bg-black hover:bg-blue-800 hover:text-white text-[#00df9a] h-[50px] w-[200px] rounded-xl font-medium ">
               ORDER TODAY
             </button>
