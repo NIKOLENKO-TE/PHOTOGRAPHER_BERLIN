8e7a08af6a55cdb20fbaef4099ecacfb9b9ec6cd
Nikolenko Tymofii <nikolenkote@gmail.com>
2023-10-21 15:48:15 +0200

    Добавил лого сайта во вкладке

--- src/components/10_test_Feature.js
+++ src/components/10_test_Feature.js
@@ -1,59 +1,80 @@
-import React, { useState } from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import featured_photo_0 from './img/Featured/0.jpg';
-import featured_photo_1 from './img/Featured/1.jpg';
-import featured_photo_2 from './img/Featured/2.jpg';
-import featured_photo_3 from './img/Featured/3.jpg';
-import featured_photo_4 from './img/Featured/4.jpg';
-import featured_photo_5 from './img/Featured/5.jpg';
-import featured_photo_6 from './img/Featured/6.jpg';
-import featured_photo_7 from './img/Featured/7.jpg';
-import featured_photo_8 from './img/Featured/8.jpg';
-import featured_photo_9 from './img/Featured/9.jpg';
+import React, { useState, useEffect, useRef } from "react";
+import "@splidejs/splide/dist/css/themes/splide-default.min.css";
+
+import { Splide } from '@splidejs/splide';
+
+import featured_photo_0 from "./img/Featured/0.jpg";
+import featured_photo_1 from "./img/Featured/1.jpg";
+import featured_photo_2 from "./img/Featured/2.jpg";
+import featured_photo_3 from "./img/Featured/3.jpg";
+import featured_photo_4 from "./img/Featured/4.jpg";
+import featured_photo_5 from "./img/Featured/5.jpg";
+import featured_photo_6 from "./img/Featured/6.jpg";
+import featured_photo_7 from "./img/Featured/7.jpg";
+import featured_photo_8 from "./img/Featured/8.jpg";
+import featured_photo_9 from "./img/Featured/9.jpg";
 
 const Featured = () => {
-    const sliders = [
-        featured_photo_1,
-        featured_photo_0,
-        featured_photo_2,
-        featured_photo_3,
-        featured_photo_4,
-        featured_photo_5,
-        featured_photo_6,
-        featured_photo_7,
-        featured_photo_8,
-        featured_photo_9,
-    ];
-    const [currentIndex, setCurrentIndex] = useState(0);
+  const sliders = [
+    featured_photo_0,
+    featured_photo_1,
+    featured_photo_2,
+    featured_photo_3,
+    featured_photo_4,
+    featured_photo_5,
+    featured_photo_6,
+    featured_photo_7,
+    featured_photo_8,
+    featured_photo_9,
+  ];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const splideRef = useRef(null);
 
-    return (
-        <div className='ssm:h-[500px] md:h-[700px] w-full py-1 px-2 relative group'>
-            <Carousel
-                useKeyboardArrows={true}
-                showStatus={false}
-                showArrows={true}
-                showThumbs={false}
-                selectedItem={currentIndex}
-                onChange={setCurrentIndex}
-                emulateTouch={true}
-                infiniteLoop={true}
-                autoPlay={true}
-                interval={10000}
-                transitionTime={1000}
-            >
-                {sliders.map((sliderItem, slideIndex) => (
-                    <div key={slideIndex} className='cursor-pointer'>
-                        <img
-                            className='h-[500px] md:h-[700px] object-cover ease-out duration-300 rounded-xl'
-                            src={sliderItem}
-                            alt={`Slide ${slideIndex}`}
-                        />
-                    </div>
-                ))}
-            </Carousel>
+  useEffect(() => {
+    const splide = new Splide(splideRef.current, {
+        type: "slide",
+        perPage: 1,
+        perMove: 1,
+        gap: 10,
+        rewind: true,
+        pagination: {
+            el: '.splide__pagination',
+            perPage: 1, // Установите perPage в 10
+            // Установите gap: 10, чтобы точки навигации были ближе друг к другу
+            gap: 1,
+            // Опция "focus" позволяет отображать точки навигации для видимых слайдов
+            focus: 'center',
+            // Опция "trimSpace" удаляет пустое пространство слева и справа от слайдов
+            trimSpace: true,
+        },
+        arrows: true,
+    });
+
+    splide.mount();
+}, []);
+
+
+
+
+  return (
+    <div className="py-1 px-2 relative group cursor-pointer rounded-xl">
+      <section id="thumbnail-carousel" ref={splideRef} className="splide">
+        <div className="splide__track rounded-2xl">
+          <ul className="splide__list">
+            {sliders.map((sliderItem, slideIndex) => (
+              <li key={slideIndex} className="splide__slide">
+                <img
+                  className="h-[700px] w-full object-cover duration-300 ease-out"
+                  src={sliderItem}
+                  alt={`Slide ${slideIndex}`}
+                />
+              </li>
+            ))}
+          </ul>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default Featured;
