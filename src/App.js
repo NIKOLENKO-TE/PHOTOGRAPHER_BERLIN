077277adb722c237672657a69cef50f5351a838f
NIKOLENKO <nikolenkote@gmail.com>
2024-08-16 22:07:53 +0200

    added spinner and lazy download

--- src/App.js
+++ src/App.js
@@ -1,19 +1,66 @@
-// App.tsx
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, Suspense, lazy, forwardRef } from "react";
 import { Toaster } from 'react-hot-toast';
+import { useInView } from 'react-intersection-observer';
+import { RotatingLines  } from 'react-loader-spinner';  // Импортируем индикатор загрузки
 import TopNav from "./components/TopNav.tsx";
-import Weddings from "./components/Weddings.tsx";
-import AboutMe from "./components/About_Me.tsx";
 import Categories from "./components/Categories.tsx";
 import Footer from "./components/Footer.tsx";
-import Restoration from "./components/Restoration.tsx";
 import BottomBanner from "./components/Bottom_banner.tsx";
-import Flowers from "./components/Flowers.tsx";
-import Childrens from "./components/Childrens.tsx";
-import Photosessions from "./components/Photosessions.tsx";
-import Hospital from "./components/Hospital.tsx";
-import AID from "./components/AID.tsx";
 import bg_image from "./components/img/1.jpg";
+
+const Weddings = lazy(() => import("./components/Weddings.tsx"));
+const AboutMe = lazy(() => import("./components/About_Me.tsx"));
+const Restoration = lazy(() => import("./components/Restoration.tsx"));
+const Flowers = lazy(() => import("./components/Flowers.tsx"));
+const Childrens = lazy(() => import("./components/Childrens.tsx"));
+const Photosessions = lazy(() => import("./components/Photosessions.tsx"));
+const Hospital = lazy(() => import("./components/Hospital.tsx"));
+const AID = lazy(() => import("./components/AID.tsx"));
+
+const LazyLoadComponent = forwardRef(({ component: Component, ...rest }, ref) => {
+  const { ref: inViewRef, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  // Объединение refs: forwarded ref и inView ref
+  const setRefs = (node) => {
+    inViewRef(node);
+    if (ref) ref.current = node;
+  };
+
+  return (
+      <div ref={setRefs} style={{ minHeight: '300px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        {inView ? (
+            <Suspense fallback={<RotatingLines
+                visible={true}
+                height="96"
+                width="96"
+                color="grey"
+                strokeWidth="5"
+                animationDuration="0.75"
+                ariaLabel="rotating-lines-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+            />} >
+              <Component {...rest} />
+            </Suspense>
+        ) : (
+            <RotatingLines
+                visible={true}
+                height="96"
+                width="96"
+                color="grey"
+                strokeWidth="5"
+                animationDuration="0.75"
+                ariaLabel="rotating-lines-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+            />
+        )}
+      </div>
+  );
+});
 
 function App() {
   const weddingRef = useRef(null);
@@ -47,8 +94,8 @@ function App() {
         targetRef = aidRef;
         break;
       case 7:
-         targetRef = hospitalRef;
-         break;
+        targetRef = hospitalRef;
+        break;
       case 8:
         targetRef = aboutMeRef;
         break;
@@ -85,27 +132,26 @@ function App() {
   };
 
   return (
-   
-    <div className="relative" style={{ overflow: "hidden" }}> 
-    <Toaster />
-      <div className="parallax-bg " style={dg_image_style} />
-      <div className="relative bg-origin-content">
-        <TopNav /> 
-        <Categories scrollToBlock={scrollToBlock} />
-        <Weddings ref={weddingRef} />
-        <Flowers ref={flowersRef} />
-        <Childrens ref={childrensRef} />
-        <Photosessions ref={photosessionsRef} /> 
-        <AID ref={aidRef} /> 
-        <Hospital ref={hospitalRef} />
-        <Restoration ref={restorationRef} />
-        <AboutMe ref={aboutMeRef} />
-        <Footer />
-        <div className="fixed bottom-0 w-full ssm:flex z-51">
-          <BottomBanner />
+      <div className="relative" style={{ overflow: "hidden" }}>
+        <Toaster />
+        <div className="parallax-bg" style={dg_image_style} />
+        <div className="relative bg-origin-content">
+          <TopNav />
+          <Categories scrollToBlock={scrollToBlock} />
+          <LazyLoadComponent component={Weddings} ref={weddingRef} />
+          <LazyLoadComponent component={Flowers} ref={flowersRef} />
+          <LazyLoadComponent component={Childrens} ref={childrensRef} />
+          <LazyLoadComponent component={Photosessions} ref={photosessionsRef} />
+          <LazyLoadComponent component={AID} ref={aidRef} />
+          <LazyLoadComponent component={Hospital} ref={hospitalRef} />
+          <LazyLoadComponent component={Restoration} ref={restorationRef} />
+          <LazyLoadComponent component={AboutMe} ref={aboutMeRef} />
+          <Footer />
+          <div className="fixed bottom-0 w-full ssm:flex z-51">
+            <BottomBanner />
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
