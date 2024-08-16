c718717b49282540a48644d7d6334318e5707e37
NIKOLENKO <nikolenkote@gmail.com>
2024-08-16 18:40:03 +0200

    // Import the Webpack module API types

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,131 +1,134 @@
-// data.ts
-function importAll(r: any) {
-  let images: { [key: string]: string } = {};
-  r.keys().map((item: string) => {
-    return images[item.replace("./", "")] = r(item);
-  });
-  return images;
+// Import the Webpack module API types
+/// <reference types="webpack-env" />
+
+function importAll(r: __WebpackModuleApi.RequireContext) {
+    let images: { [key: string]: string } = {};
+    r.keys().forEach((item: string) => {
+        images[item.replace("./", "")] = r(item);
+    });
+    return images;
 }
+
 const weddingVerticalImages = importAll(
-  require.context("../components/img/Wedding/verticalWeddings", false, /\.(png|jpe?g|svg)$/)
+    require.context("../components/img/Wedding/verticalWeddings", false, /\.(png|jpe?g|svg)$/)
 );
 const weddingHorizontalImages = importAll(
-  require.context("../components/img/Wedding/horizontalWeddings", false, /\.(png|jpe?g|svg)$/)
+    require.context("../components/img/Wedding/horizontalWeddings", false, /\.(png|jpe?g|svg)$/)
 );
 const flowersImages = importAll(
-  require.context("../components/img/Flowers", false, /\.(png|jpe?g|svg)$/)
+    require.context("../components/img/Flowers", false, /\.(png|jpe?g|svg)$/)
 );
 const childrensImages = importAll(
-  require.context("../components/img/Childrens", false, /\.(png|jpe?g|svg)$/)
+    require.context("../components/img/Childrens", false, /\.(png|jpe?g|svg)$/)
 );
 const restorationImages = importAll(
-  require.context("../components/img/Restoration", false, /\.(png|jpe?g|svg)$/)
+    require.context("../components/img/Restoration", false, /\.(png|jpe?g|svg)$/)
 );
 const photosessionsHorizontalImages = importAll(
-  require.context("../components/img/Photosessions/horizontalPhotos", false, /\.(png|jpe?g|svg)$/)
+    require.context("../components/img/Photosessions/horizontalPhotos", false, /\.(png|jpe?g|svg)$/)
 );
 const photosessionsVerticalImages = importAll(
-  require.context("../components/img/Photosessions/verticalPhotos", false, /\.(png|jpe?g|svg)$/)
+    require.context("../components/img/Photosessions/verticalPhotos", false, /\.(png|jpe?g|svg)$/)
 );
 const hospitalImages = importAll(
-  require.context("../components/img/Hospital", false, /\.(png|jpe?g|svg)$/)
+    require.context("../components/img/Hospital", false, /\.(png|jpe?g|svg)$/)
 );
 const aidImages = importAll(
-  require.context("../components/img/AID", false, /\.(png|jpe?g|svg)$/)
+    require.context("../components/img/AID", false, /\.(png|jpe?g|svg)$/)
 );
 const restorationIconsImages = importAll(
-  require.context(
-    "../components/img/Restoration/icons",
-    false,
-    /\.(png|jpe?g|svg)$/
-  )
+    require.context("../components/img/Restoration/icons", false, /\.(png|jpe?g|svg)$/)
 );
 const categoriesImages = importAll(
-  require.context("../components/img/categories", false, /\.(png|jpe?g|svg)$/)
+    require.context("../components/img/categories", false, /\.(png|jpe?g|svg)$/)
 );
+
 const categoriesData = [
-  { id: 1, title: "Weddings", shortTitle: "Weddings", imgFile: "1_weddings.jpg" },
-  { id: 2, title: "Flowers", shortTitle: "Flowers", imgFile: "2_flowers_1.jpg" },
-  { id: 3, title: "Children's parties", shortTitle: "Childrens", imgFile: "4_childrens_parties.jpg" },
-  { id: 4, title: "Restorations", shortTitle: "Restorations", imgFile: "5_restorations.jpg" },
-  { id: 5, title: "Photosessions", shortTitle: "Photosessions", imgFile: "3_photosessions.jpg" },
-  { id: 6, title: "Humanitarian AID", shortTitle: "AID", imgFile: "6_humanitarian_aid.jpg" },
-  { id: 7, title: "War medicine", shortTitle: "War medicine", imgFile: "7_war_medicine.jpg" },
-  { id: 8, title: "About me", shortTitle: "About me", imgFile: "nikolenkote.jpg" },
+    { id: 1, title: "Weddings", shortTitle: "Weddings", imgFile: "1_weddings.jpg" },
+    { id: 2, title: "Flowers", shortTitle: "Flowers", imgFile: "2_flowers_1.jpg" },
+    { id: 3, title: "Children's parties", shortTitle: "Childrens", imgFile: "4_childrens_parties.jpg" },
+    { id: 4, title: "Restorations", shortTitle: "Restorations", imgFile: "5_restorations.jpg" },
+    { id: 5, title: "Photosessions", shortTitle: "Photosessions", imgFile: "3_photosessions.jpg" },
+    { id: 6, title: "Humanitarian AID", shortTitle: "AID", imgFile: "6_humanitarian_aid.jpg" },
+    { id: 7, title: "War medicine", shortTitle: "War medicine", imgFile: "7_war_medicine.jpg" },
+    { id: 8, title: "About me", shortTitle: "About me", imgFile: "nikolenkote.jpg" },
 ];
+
 export const categoryPhotos = categoriesData.map((category) => ({
-  id: category.id,
-  title: category.title,
-  shortTitle: category.shortTitle,
-  img: categoriesImages[category.imgFile],
+    id: category.id,
+    title: category.title,
+    shortTitle: category.shortTitle,
+    img: categoriesImages[category.imgFile],
 }));
 
 export const hospitalPhotos = Object.keys(hospitalImages).map((key, index) => ({
-  id: index,
-  title: `hospitalPhoto${index}`,
-  img: hospitalImages[key],
+    id: index,
+    title: `hospitalPhoto${index}`,
+    img: hospitalImages[key],
 }));
 
 export const flowersPhotos = Object.keys(flowersImages).map((key, index) => ({
-  id: index,
-  title: `flowersPhoto${index}`,
-  img: flowersImages[key],
+    id: index,
+    title: `flowersPhoto${index}`,
+    img: flowersImages[key],
 }));
 
 export const childrensPhotos = Object.keys(childrensImages).map(
-  (key, index) => ({
-    id: index,
-    title: `childrensPhoto${index}`,
-    img: childrensImages[key],
-  })
+    (key, index) => ({
+        id: index,
+        title: `childrensPhoto${index}`,
+        img: childrensImages[key],
+    })
 );
+
 export const aidPhotos = Object.keys(aidImages).map(
-  (key, index) => ({
-    id: index,
-    title: `aidPhoto${index}`,
-    img: aidImages[key],
-  })
+    (key, index) => ({
+        id: index,
+        title: `aidPhoto${index}`,
+        img: aidImages[key],
+    })
 );
+
 export const restoredPhotos = Object.keys(restorationImages).map(
-  (key, index) => ({
-    id: index,
-    title: `Restore${index}`,
-    img: restorationImages[key],
-  })
+    (key, index) => ({
+        id: index,
+        title: `Restore${index}`,
+        img: restorationImages[key],
+    })
 );
 
 export const weddingPhotosVertical = Object.keys(weddingVerticalImages).map((key, index) => ({
-  id: index,
-  title: `weddingPhoto${index}`,
-  img: weddingVerticalImages[key],
+    id: index,
+    title: `weddingPhoto${index}`,
+    img: weddingVerticalImages[key],
 }));
 
 export const weddingHorizontal = Object.keys(weddingHorizontalImages).map((key, index) => ({
-  id: index,
-  title: `weddingPhoto${index}`,
-  img: weddingHorizontalImages[key],
+    id: index,
+    title: `weddingPhoto${index}`,
+    img: weddingHorizontalImages[key],
 }));
 
 export const photosessionsHorizontal = Object.keys(photosessionsHorizontalImages).map(
-  (key, index) => ({
-    id: index,
-    title: `photosessionsHorizontal${index}`,
-    img: photosessionsHorizontalImages[key],
-  })
+    (key, index) => ({
+        id: index,
+        title: `photosessionsHorizontal${index}`,
+        img: photosessionsHorizontalImages[key],
+    })
 );
 
 export const photosessionsVertical = Object.keys(photosessionsVerticalImages).map(
-  (key, index) => ({
-    id: index,
-    title: `photosessionsVertical${index}`,
-    img: photosessionsVerticalImages[key],
-  })
+    (key, index) => ({
+        id: index,
+        title: `photosessionsVertical${index}`,
+        img: photosessionsVerticalImages[key],
+    })
 );
 
 export const orderRestorePhotos = Object.keys(restorationIconsImages).map(
-  (key, index) => ({
-    id: index,
-    title: `orderRestorePhotos${index}`,
-    img: restorationIconsImages[key],
-  })
+    (key, index) => ({
+        id: index,
+        title: `orderRestorePhotos${index}`,
+        img: restorationIconsImages[key],
+    })
 );
--- src/i18n.ts
+++ src/i18n.ts
@@ -3,31 +3,47 @@ import * as i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { isDevelopmentMode } from './utils';
-
+/**
+ * Использует i18next-browser-languagedetector для обнаружения языка пользователя.
+ * @see {@link https://github.com/i18next/i18next-browser-languageDetector}
+ *
+ */
 import deTranslations from './locales/de.json';
 import uaTranslations from './locales/ua.json';
 import enTranslations from './locales/en.json';
 
+
 i18n
-	// detect user language
-	// learn more: https://github.com/i18next/i18next-browser-languageDetector
+
 	.use(LanguageDetector)
-	// pass the i18n instance to react-i18next.
+	/**
+	 * Передает экземпляр i18n в react-i18next для интеграции с React.
+	 * @see {@link https://react.i18next.com/}
+	 */
 	.use(initReactI18next)
-	// init i18next
-	// for all options read: https://www.i18next.com/overview/configuration-options
+	/**
+	 * Инициализирует экземпляр i18next с предоставленными параметрами конфигурации.
+	 * @param {Object} options - Параметры конфигурации для i18next.
+	 * @param {boolean} options.debug - Включает режим отладки, если включен режим разработки.
+	 * @param {string} options.fallbackLng - Язык по умолчанию, если язык пользователя недоступен.
+	 * @param {Object} options.interpolation - Параметры интерполяции для i18next.
+	 * @param {boolean} options.interpolation.escapeValue - Отключает экранирование для React, так как оно выполняется по умолчанию.
+	 * @param {string} options.defaultNS - Пространство имен по умолчанию для переводов.
+	 * @param {Object} options.resources - Ресурсы перевода для различных языков.
+	 * @param {Object} options.resources.en - Ресурсы перевода для английского языка.
+	 * @param {Object} options.resources.ua - Ресурсы перевода для украинского языка.
+	 * @param {Object} options.resources.de - Ресурсы перевода для немецкого языка.
+	 */
 	.init({
 		debug: isDevelopmentMode(),
 		fallbackLng: 'ua',
 		interpolation: {
-			escapeValue: false, // not needed for react as it escapes by default
+			escapeValue: false, // не нужно для react, так как экранирование выполняется по умолчанию
 		},
 		defaultNS: '',
 		resources: {
-			en: enTranslations, // Use translations for English
-			ua: uaTranslations, // Use translations for Russian
-			de: deTranslations, // Use translations for German
+			en: enTranslations, // Использовать переводы для английского языка
+			ua: uaTranslations, // Использовать переводы для украинского языка
+			de: deTranslations, // Использовать переводы для немецкого языка
 		},
 	});
-
-export default i18n;
